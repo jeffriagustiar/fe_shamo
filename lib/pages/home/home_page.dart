abf6697938ff88b2576413c9a49e51c88aabf001
Jeffri AGustiar <jeffriagustiar@gmail.com>
2022-07-31 09:52:06 -0700

    15 Home Page Products UI

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shamo/theme.dart';
+import 'package:shamo/widgets/product_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -179,10 +180,54 @@ class HomePage extends StatelessWidget {
       );
     }
 
+    Widget popularProductTitle(){
+      return Container(
+        margin: EdgeInsets.only(
+          top: defaultMargin,
+          right: defaultMargin,
+          left: defaultMargin
+        ),
+        child: Text(
+          'Popular Products',
+          style: primaryTextStyle.copyWith(
+            fontSize: 22,
+            fontWeight: semibold
+          ),
+        ),
+      );
+    }
+
+    Widget popularProducts(){
+      return Container(
+        margin: EdgeInsets.only(
+          top: 14,
+        ),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+
+              SizedBox(width: defaultMargin,),
+
+              Row(
+                children: [
+                  ProductCard(),
+                  ProductCard(),
+                  ProductCard(),
+                ],
+              )
+            ],
+          ),
+        ),
+      );
+    }
+
     return ListView(
       children: [
         header(),
         categories(),
+        popularProductTitle(),
+        popularProducts(),
       ],
     );
   }
--- lib/theme.dart
+++ lib/theme.dart
@@ -15,12 +15,14 @@ Color primaryTextColor = Color(0xffF1F0F2);
 Color secondTextColor = Color(0xff999999);
 Color subtitleColor = Color(0xff504F5E);
 Color transparantColor = Colors.transparent;
+Color blackColor = Color(0xff2E2E2E);
 
 TextStyle primaryTextStyle = GoogleFonts.poppins(color: primaryTextColor);
 TextStyle secondTextStyle = GoogleFonts.poppins(color: secondTextColor);
 TextStyle priceTextStyle = GoogleFonts.poppins(color: priceColor);
 TextStyle subTextStyle = GoogleFonts.poppins(color: subtitleColor);
 TextStyle purpleTextStyle = GoogleFonts.poppins(color: primaryColor);
+TextStyle blackTextStyle = GoogleFonts.poppins(color: blackColor);
 
 FontWeight light = FontWeight.w300;
 FontWeight regular = FontWeight.w400;
