781903055425b2d74d7bf56bc27edb57f6168703
Jeffri AGustiar <jeffriagustiar@gmail.com>
2022-07-31 09:07:00 -0700

    13 Home Page UI

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,12 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Page'),
+
+    Widget header(){
+      return Container(
+        margin: EdgeInsets.only(
+          top: defaultMargin,
+          left: defaultMargin,
+          right: defaultMargin
+        ),
+
+        child: Row(
+          children: [
+
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hello, Alex',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 24,
+                      fontWeight: semibold
+                    ),
+                  ),
+                  Text(
+                    '@alexkeinn',style: subTextStyle.copyWith(
+                      fontSize: 16,
+                    ),
+                  )
+                ],
+              ),
+            ),
+
+            Spacer(),
+
+            Container(
+              width: 54,
+              height: 54,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(image: AssetImage('assets/profile_image.png'))
+              ),
+            )
+
+          ],
+        ),
+
+      );
+    }
+
+    return ListView(
+      children: [
+        header(),
+      ],
     );
   }
 }
