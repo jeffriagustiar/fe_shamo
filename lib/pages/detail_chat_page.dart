d917b9d2cc7afe301d3a0dc99ead75c8e83ce77d
Jeffri AGustiar <jeffriagustiar@gmail.com>
2022-07-31 22:48:08 -0700

    21 Chat Page Input 2 UI

--- lib/pages/detail_chat_page.dart
+++ lib/pages/detail_chat_page.dart
@@ -43,35 +43,95 @@ class DetailChatPage extends StatelessWidget {
         );
     }
 
+    Widget productPriview(){
+      return Container(
+        height: 74,
+        width: 225,
+        margin:  EdgeInsets.only(bottom: 20),
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: backgroundColor5,
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(
+            color: primaryColor
+          )
+        ),
+
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset('assets/sepatu1bg.png',width: 54,)
+            ),
+            SizedBox(width: 10,),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'COURT VISIO...',
+                    style: primaryTextStyle,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(height: 2,),
+                  Text(
+                    '\$57,15',
+                    style: priceTextStyle.copyWith(
+                      fontWeight: medium
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Image.asset('assets/x_btn.png',width: 22,)
+          ],
+        ),
+
+      );
+    }
+
     Widget chatInput(){
       return Container(
         margin: EdgeInsets.all(20),
-        child: Row(
+
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Expanded(
-              child: Container(
-                height: 45,
-                padding: EdgeInsets.symmetric(
-                  horizontal: 16, 
-                ),
-                decoration: BoxDecoration(
-                  color: backgroundColor4,
-                  borderRadius: BorderRadius.circular(12)
-                ),
-                child: Center(
-                  child: TextFormField(
-                    decoration: InputDecoration.collapsed(
-                      hintText: 'Type Message...',
-                      hintStyle: subTextStyle
+
+            productPriview(),
+
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 45,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 16, 
+                    ),
+                    decoration: BoxDecoration(
+                      color: backgroundColor4,
+                      borderRadius: BorderRadius.circular(12)
+                    ),
+                    child: Center(
+                      child: TextFormField(
+                        decoration: InputDecoration.collapsed(
+                          hintText: 'Type Message...',
+                          hintStyle: subTextStyle
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
+
+                SizedBox(width: 20,),
+
+                Image.asset('assets/Send_Button.png',width: 45,)
+
+              ],
             ),
-
-            SizedBox(width: 20,),
-
-            Image.asset('assets/Send_Button.png',width: 45,)
 
           ],
         ),
