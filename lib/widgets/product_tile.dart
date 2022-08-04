78abaf0af34571b30d3ec53a5c9132cee1cf0f0c
Jeffri AGustiar <jeffriagustiar@gmail.com>
2022-08-03 21:25:36 -0700

    31 Product Page header nav ui

--- lib/widgets/product_tile.dart
+++ lib/widgets/product_tile.dart
@@ -6,54 +6,59 @@ class ProductTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(
-        left: defaultMargin,
-        right: defaultMargin,
-        bottom: defaultMargin
-      ),
-      child: Row(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: Image.asset(
-              'assets/sepatu1bg.png',
-              width: 120,
-              height: 120,
-              fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, '/product');
+      },
+      child: Container(
+        margin: EdgeInsets.only(
+          left: defaultMargin,
+          right: defaultMargin,
+          bottom: defaultMargin
+        ),
+        child: Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset(
+                'assets/sepatu1bg.png',
+                width: 120,
+                height: 120,
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          SizedBox(width: 12,),
-
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Football',
-                  style: secondTextStyle.copyWith(
-                    fontSize: 12
+            SizedBox(width: 12,),
+    
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Football',
+                    style: secondTextStyle.copyWith(
+                      fontSize: 12
+                    ),
                   ),
-                ),
-                SizedBox(height: 6,),
-                Text(
-                  'Predator 20.3 firm Ground',
-                  style: primaryTextStyle.copyWith(
-                    fontSize: 16,
-                    fontWeight: semibold
+                  SizedBox(height: 6,),
+                  Text(
+                    'Predator 20.3 firm Ground',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: semibold
+                    ),
                   ),
-                ),
-                SizedBox(height: 6,),
-                Text(
-                  '\$68,47',
-                  style: priceTextStyle.copyWith(
-                    fontWeight: medium
-                  ),
-                )
-              ],
+                  SizedBox(height: 6,),
+                  Text(
+                    '\$68,47',
+                    style: priceTextStyle.copyWith(
+                      fontWeight: medium
+                    ),
+                  )
+                ],
+              )
             )
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
