e7220d00c781a2fe07efd01e924930cd5ebd4674
Jeffri AGustiar <jeffriagustiar@gmail.com>
2022-08-03 22:42:38 -0700

    35 Product Page content 3 ui

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -253,6 +253,53 @@ class _ProductPageState extends State<ProductPage> {
               ),
             ),
 
+            //? Note. Buttons
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.all(defaultMargin),
+              child: Row(
+                children: [
+
+                  Container(
+                    height: 54,
+                    width: 54,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
+                          'assets/Button_Chat.png'
+                        )
+                      )
+                    ),
+                  ),
+
+                  SizedBox(width: 16,),
+
+                  Expanded(
+                    child: Container(
+                      height: 54,
+                      child: TextButton(
+                        onPressed: () {}, 
+                        style: TextButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12)
+                          ),
+                          backgroundColor: primaryColor
+                        ),
+                        child: Text(
+                          'Add to Cart',
+                          style: primaryTextStyle.copyWith(
+                            fontSize: 16,
+                            fontWeight: bold
+                          ),
+                        )
+                      ),
+                    ),
+                  )
+
+                ],
+              ),
+            )
+
           ],
         ),
       );
