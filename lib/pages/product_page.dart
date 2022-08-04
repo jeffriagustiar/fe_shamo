c97454f80e1f5a56f85c29d67f833ef490c33922
Jeffri AGustiar <jeffriagustiar@gmail.com>
2022-08-03 22:10:51 -0700

    33 Product Page content 1 ui

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -88,11 +88,114 @@ class _ProductPageState extends State<ProductPage> {
       );
     }
 
+    Widget content(){
+      return Container(
+        margin: EdgeInsets.only(top: 17),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(24)
+          ),
+          color: backgroundColor1
+        ),
+        child: Column(
+          children: [
+
+            //? Note : Header Content
+            Container(
+              margin: EdgeInsets.only(top: defaultMargin,left: defaultMargin,right: defaultMargin),
+              child: Row(
+                children: [
+
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'TERREX URBAN LOW',
+                          style: primaryTextStyle.copyWith(
+                            fontSize: 18,
+                            fontWeight: semibold
+                          ),
+                        ),
+                        Text(
+                          'Hiking',
+                          style: secondTextStyle.copyWith(
+                            fontSize: 12
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+
+                  Image.asset('assets/Love_Button_blck.png',width: 46,)
+
+                ],
+              ),
+            ),
+
+            //? Note. Price Content
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.all(16),
+              margin: EdgeInsets.only(top: 20,left: defaultMargin,right: defaultMargin),
+              decoration: BoxDecoration(
+                color: backgroundColor2,
+                borderRadius: BorderRadius.circular(4)
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Price starts from',
+                    style: primaryTextStyle,
+                  ),
+                  Text(
+                    '\$143,98',
+                    style: priceTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: semibold
+                    ),
+                  )
+                ],
+              ),
+            ),
+
+            //? Note. Description
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.only(top: defaultMargin, right: defaultMargin,left: defaultMargin),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Description',
+                    style: primaryTextStyle.copyWith(
+                      fontWeight: medium
+                    ),
+                  ),
+                  SizedBox(height: 12,),
+                  Text(
+                    'Unpaved trails and mixed surfaces are easy when you have the traction and support you need. Casual enough for the daily commute.',
+                    style: subTextStyle.copyWith(
+                      fontWeight: light,
+                    ),
+                    textAlign: TextAlign.justify,
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: backgroundColor6,
       body: ListView(
         children: [
           header(),
+          content()
         ],
       )
     );
