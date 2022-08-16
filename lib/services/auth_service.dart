3acbb17e76f70fa18a6d872683648bcdf96d50a8
Jeffri AGustiar <jeffriagustiar@gmail.com>
2022-08-15 22:02:22 -0700

    59 Checkout service dart

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -5,9 +5,9 @@ import 'package:http/http.dart' as http;
 
 class AuthService {
   // String baseUrl = 'http://127.0.0.1:8000/api';
-  String baseUrl = 'http://10.0.2.2:8000/api';
+  // String baseUrl = 'http://10.0.2.2:8000/api';
   // String baseUrl = 'http://192.168.43.126:8000/api';
-  // String baseUrl = 'https://shamo-backend.buildwithangga.id/api';
+  String baseUrl = 'https://shamo-backend.buildwithangga.id/api';
 
   Future<UserModel> register({
     required String name,
---
+++ lib/services/transaction_service.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import '../models/cart_model.dart';
+
+class TransactionService {
+  // String baseUrl = 'http://127.0.0.1:8000/api';
+  // String baseUrl = 'http://10.0.2.2:8000/api';
+  // String baseUrl = 'http://192.168.43.126:8000/api';
+  String baseUrl = 'https://shamo-backend.buildwithangga.id/api';
+
+  Future<bool> checkout(
+      String token, List<CartModel> carts, double totalPrice) async {
+    var url = Uri.parse('$baseUrl/checkout');
+    var headers = {
+      'Content-Type': 'application/json',
+      'Authorization': token,
+    };
+
+    var body = jsonEncode(
+      {
+        'address': 'test',
+        'items': carts
+            .map((cart) => {
+                  'id': cart.product.id,
+                  'quantity': cart.quantity,
+                })
+            .toList(),
+        'status': "PENDING",
+        'total_price': totalPrice,
+        'shipping_price': 0
+      },
+    );
+
+    var response = await http.post(url, headers: headers, body: body);
+
+    print(response.body);
+
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      throw Exception('Gagal, Melakukan Checkout');
+    }
+  }
+}
